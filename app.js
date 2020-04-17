b2b94cfa3787c94dca5a9c21203bb78667eba122
finkelmanje <38955583+finkelmanje@users.noreply.github.com>
2020-04-16 22:01:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict'
+var n1
+var n2
+var n3
+let today = new Date();
+let formatDate = today.toDateString();
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
+function fn1() {
+    n1 = document.getElementById("num1").value;
+}
+
+function fn2() {
+    n2 = document.getElementById("num2").value;
+}
+
+function fn3() {
+    n3 = document.getElementById("num3").value;
+    var arr1 = [Number(n1), Number(n2), Number(n3)];
+    arr1.sort();
+    document.write(arr1);
+    return arr1;
+}
